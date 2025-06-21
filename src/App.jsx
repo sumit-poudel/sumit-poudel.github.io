50b412ab2611c34d8642ef48a8a148fdc9bdf443
sumitpoudel <sumitpoudel325@gmail.com>
2025-06-21 14:14:54 +0545

    working animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { ChatBubble } from "./components/chatBubble"
+
+
 const App=()=>{
+
+let things=["Hello! Mero nam sumit ho.",
+            "Ma birendra ma padhxu",
+            "I am doing BCA",
+            "Coding is fun",
+            "Penguins hate me T_T"
+]
+ 
+
 return (<div className="bg-white dark:bg-gray-800 p-8 h-screen ">
- <ChatBubble text="Hello! Mero nam sumit ho." />
+  {
+    things.map(element =>
+     <ChatBubble text={element} />
+    )
+  }
   </div>)
 
 }
--- src/components/ChatBubble.jsx
+++ src/components/ChatBubble.jsx
@@ -1,10 +1,43 @@
-import user from "../assets/user-circles-set.png"
-export const ChatBubble = ({text}) => {
+import * as motion from "motion/react-client";
+import user from "../assets/user-circles-set.png";
+import { AnimatePresence } from "motion/react";
+
+export const ChatBubble = ({ text }) => {
   return (
-    <div className=" flex items-center gap-2" >
-    <img src={user} className=" w-7 h-7 p-0 m-0 " />
-    <div className="py-1 px-5 bg-slate-300  dark:bg-blue-500 rounded-full w-fit dark:text-white flex-none " >  
-      {text} </div>
+    <div className="flex flex-col w-full relative h-fit ">
+      <AnimatePresence>
+        <motion.div
+          className="flex items-center gap-2 my-5"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+        >
+          <img src={user} className="w-7 h-7 p-0 m-0" />
+        
+        <motion.div
+          className="py-1 px-5 bg-slate-300 dark:bg-blue-500 rounded-full w-fit dark:text-white flex-none"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+           whileHover={{
+              marginLeft:30,
+              scale: [null,1.3],
+              transition: {
+                duration: 0.4,
+                times: [0, 1],
+                ease: ["easeInOut", "easeOut"],
+              },
+            }}
+          transition={{
+            delay: 0.1,
+            duration: 0.4,
+            type: "spring",
+            stiffness: 150,
+            ease: "easeOut"
+          }}
+        >
+          {text}
+        </motion.div>
+        </motion.div>
+      </AnimatePresence>
     </div>
-  )
-}
+  );
+};
