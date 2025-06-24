b11a72688c0907c7d4716d8bf7f10c6033cc62a9
sumitpoudel <sumitpoudel325@gmail.com>
2025-06-24 13:06:39 +0545

    site map

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(),react(),Sitemap({hostname: 'https://sumit-poudel.com.np'})],
   server: {
     open: true,
     port: 3000,
