fd0766528d721a9fbc9d39be53fbdbafa9324257
sumitpoudel <sumitpoudel325@gmail.com>
2025-06-26 00:25:02 +0545

    seo ko lagi X_X

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -5,7 +5,7 @@ const Top=()=>{
 <div className="flex pl-3 items-center gap-4 dark:shadow-xl dark:border-none inset-shadow-2xs w-full basis-3/40 border-gray-200 border-b-1 " >
         <a href="https://github.com/sumit-poudel" target="_blank" rel="noopener noreferrer"  className="flex items-center gap-2" >
           <img src={logo} className="w-10 h-10" />
-          <p className="dark:text-white" >Sumit Poudel</p>
+          <h1 className="dark:text-white" >Sumit Poudel</h1>
         </a>
         </div>
     )
